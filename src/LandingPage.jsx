ce5e838712577bed927cb01d4caff2ba8752fce2
kry23 <infest.redstar@gmail.com>
2023-03-31 15:36:52 +0300

    footer added(without js)

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Carousel from './Components/Carousel'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 export default function LandingPage() {
   return (
     <>
       <Header />
       <Carousel />
+      <Footer />
     </>
   )
 }
