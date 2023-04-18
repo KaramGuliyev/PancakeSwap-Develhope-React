5769699b594d5b858d9e59482c08faa2d564f7eb
Gabriel <Kryemadhigabriel@gmail.com>
2023-04-18 11:40:16 +0200

    All Cards added, to add responsivenes

--- src/Components/TeamsBody.jsx
+++ src/Components/TeamsBody.jsx
@@ -19,9 +19,23 @@ export default function TeamsBody() {
           <TeamCard
             rank="1"
             teamName="Syrup Storm"
-            logo={"/assets/teams-syrup-storm.png"}
-            moto="The storm's a-comin! Watch out! These bulls are stampeding in a
-            syrupy surge!"
+            members="190,243"
+            logo={"/assets/team-syrup-storm.png"}
+            moto="The storm's a-comin! Watch out! These bulls are stampeding in a syrupy surge!"
+          />
+          <TeamCard
+            rank="2"
+            teamName="Fearsome Flippers"
+            members="170,822"
+            logo={"/assets/team-fearsome-flippers.png"}
+            moto="The flippening is coming. Don't get in these bunnies' way, or you'll get flipped, too!"
+          />
+          <TeamCard
+            rank="3"
+            teamName="Chaotic Cakers"
+            members="343,806"
+            logo={"/assets/team-chaotic-cakers.png"}
+            moto="Can you stand the heat? Stay out of the kitchen or you might get burned to a crisp!"
           />
         </div>
       </div>
