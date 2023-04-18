7c70082eff872ee950d6afa01fd786bf0d179816
Karam Guliyev <karamguliyev10@gmail.com>
2023-04-18 22:50:24 +0300

    Cleaning

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,4 @@
-function removeScripts() {
-  // Select the head element
-  var head = document.getElementsByTagName("head")[0];
-
-  // Select all script elements within the head
-  var scripts = head.getElementsByTagName("script");
-
-  // Remove each script element
-  for (var i = scripts.length - 1; i >= 0; i--) {
-    console.log(scripts[i]);
-    head.removeChild(scripts[i]);
-  }
-}
-
 function loadScript(src) {
-  removeScripts();
   setTimeout(() => {
     var script = document.createElement("script");
     script.src = src;
