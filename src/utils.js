48bd3e17d2547d0585e8cc068bf58a0afb167610
Karam Guliyev <karamguliyev10@gmail.com>
2023-04-19 13:32:10 +0300

    utils bug fix

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,9 @@
 function loadScript(src) {
-  setTimeout(() => {
     var script = document.createElement("script");
     script.src = src;
     script.async = true;
     document.head.appendChild(script);
     return script;
-  }, 500);
 }
 
 export { loadScript };
